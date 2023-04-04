533ac8a0cdfbb2ade4a277c063ac83ebe26e4e62
bduha184 <bduha_184@yahoo.co.jp>
2023-04-04 23:46:01 +0900

    task項目追加

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,22 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Task from "./components/Task.vue";
+import Carendar from "./components/Carendar.vue";
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="wrapper">
+    <div id="gantt-header" class="h-12 p-2 flex items-center">
+      <h1 class="text-xl font-bold">ガントチャート</h1>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div id="gantt-content" class="flex">
+      <div id="gantt-task">
+        <Task />
+      </div>
+      <div id="gantt-calendar">
+        <Carendar />
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
---
+++ src/components/Task.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <div
+    id="gantt-task-title"
+    class="flex items-center bg-green-600 text-white h-20"
+  >
+    <div
+      class="border-t border-r border-b flex items-center justify-center font-bold text-xs w-48 h-full"
+    >
+      タスク
+    </div>
+    <div
+      class="border-t border-r border-b flex items-center justify-center font-bold text-xs w-24 h-full"
+    >
+      開始日
+    </div>
+    <div
+      class="border-t border-r border-b flex items-center justify-center font-bold text-xs w-24 h-full"
+    >
+      完了期限日
+    </div>
+    <div
+      class="border-t border-r border-b flex items-center justify-center font-bold text-xs w-16 h-full"
+    >
+      担当
+    </div>
+    <div
+      class="border-t border-r border-b flex items-center justify-center font-bold text-xs w-12 h-full"
+    >
+      進捗
+    </div>
+  </div>
+</template>
